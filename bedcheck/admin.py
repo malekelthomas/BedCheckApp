f53529322d1773f06a58263b60753c73542d366c
maleke <malekelthomas97@gmail.com>
2020-08-09 04:46:08 -0400

    import forms

--- bedcheck/admin.py
+++ bedcheck/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Client
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
 admin.site.register(Client)
