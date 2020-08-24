0edd9d2e136cc42db93986873a5afde41969b519
maleke <malekelthomas97@gmail.com>
2020-08-24 16:57:46 -0400

    Redirect edit, client view styling, sig path

--- bedcheck/views.py
+++ bedcheck/views.py
@@ -50,7 +50,7 @@ def register_view(request, *args, **kwargs):
             username = form.cleaned_data.get('username')
             messages.success(request, f"New account created: {username}")
             login(request,user)
-            return redirect("bedcheck:home")
+            return redirect("/")
         else:
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
@@ -142,7 +142,7 @@ def single_client_view(request, caresID, *args, **kwargs):
                     f.write(base64.decodebytes(str_as_bytes))
                     f.close()
             	   
-            client.signature = str(this_client_signatures_path)[str(this_client_signatures_path).index("media\\")+len("media\\"):]+"\sig.png" #have to handle slashes on different platforms
+            client.signature = str(this_client_signatures_path)[str(this_client_signatures_path).index("media/")+len("media/"):]+"/sig.png" #have to handle slashes on different platforms
             print(client.signature)
             client.save()
             return redirect("/roster")
