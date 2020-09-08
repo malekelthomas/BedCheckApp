7f951a6ee1765ed8ef4bfbdeee63774db96c2229
maleke <malekelthomas97@gmail.com>
2020-09-08 17:39:44 -0400

    Updated settings

--- BedCheckApp/settings.py
+++ BedCheckApp/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
+'''STATICFILES_DIRS=(
     STATIC_DIR,
-)
+)'''
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'BedCheckApp/media')
